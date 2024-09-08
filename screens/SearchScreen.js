8f421dfc8e828bda66402b110b4157f56ffa9747
Samet <sametozturk973@gmail.com>
2024-09-08 18:55:33 +0300

    Search screen created template

---
+++ screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import SearchBar from "../components/SearchBar";
+
+export default function SearchScreen() {
+  return (
+    <View>
+      <SearchBar />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
