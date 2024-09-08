cd898abb636642551e11f4b16afbf19991e3f1b7
Samet <sametozturk973@gmail.com>
2024-09-08 18:54:43 +0300

    Search bar template

---
+++ components/SearchBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+export default function SearchBar() {
+  return (
+    <View style={styles.searchBar}>
+      <AntDesign
+        style={styles.searchBarIcon}
+        name="search1"
+        size={24}
+        color="black"
+      />
+      <TextInput
+        style={styles.searchBarPlaceHolder}
+        placeholder="Market veya ürün ara"
+        autoComplete={false}
+        autoCapitalize="none"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  searchBar: {
+    backgroundColor: "#DCDCDC",
+    height: 40,
+    borderRadius: 10,
+    marginHorizontal: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 15,
+  },
+  searchBarIcon: {
+    marginLeft: 10,
+  },
+  searchBarPlaceHolder: {
+    marginLeft: 10,
+    fontSize: 20,
+  },
+});
