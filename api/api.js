c9d1fc9da64296b2b25b72cc6850283c7ce6afe0
Samet <sametozturk973@gmail.com>
2024-09-16 19:23:14 +0300

    Import URL

--- api/api.js
+++ api/api.js
@@ -1,9 +1,10 @@
-export default axios.create({
-  baseURL: "https://api.yelp.com/v3/businesses",
-  headers: {
-    Authorization:
-      "Bearer aETOPuVSQ-luGy16YjLMitcWYtZh4W9pRa8Z8hk5Tz42JZA2f2UPY0ve9r7XDLnrlfVrbVqqJ-MagxP1AugW2wZgFVw2cl2ci8tq0UnFZRzUsT7_x_0-7hVa4F-RZHYx",
-  },
+import axios from "axios";
+import { URL } from "@env";
+
+console.log("API_URL:", URL);
+
+const api = axios.create({
+  baseURL: URL,
 });
 
-//"https://dummyjson.com/c/e006-6a33-4685-8887"
+export default api;
