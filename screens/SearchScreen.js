102f586e95190fb5e9aa6f94d101c9bd602c1aee
Samet <sametozturk973@gmail.com>
2024-09-16 23:24:55 +0300

    Show all markets on screen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
+import useResults from "../hooks/useResults";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMarkets } from "../store/thunks/marketThunks";
 
 export default function SearchScreen() {
+  const dispatch = useDispatch();
+  const { markets, status, error } = useSelector((state) => state.market);
+
+  useEffect(() => {
+    dispatch(fetchMarkets());
+  }, [dispatch]);
+
+  if (status === "loading") {
+    return <Text>Loading...</Text>;
+  }
+
+  if (status === "failed") {
+    return <Text>Error: {error}</Text>;
+  }
+
   return (
     <View>
       <SearchBar />
+      {markets.map((market) => (
+        <Text key={market.id}>{market.name}</Text>
+      ))}
     </View>
   );
 }
